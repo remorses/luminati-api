c4c3a0e0e2bad4e74a3a81ee13bd6ca3535f1734
remorses <beats.by.morse@gmail.com>
2019-10-08 20:15:18 +0200

    format

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,70 @@
-
 import fetch from 'node-fetch'
-import { log, parse } from './support'
+import { log, parse } from './support'
 
-
-
-
-const mapErrorToJson = async res => {
+const mapErrorToJson = async (res) => {
     const text = await res.text()
     const data = parse(text)
-        .then(data => ({
+        .then((data) => ({
             ...data,
             ok: true
         }))
-        .catch(e => ({
+        .catch((e) => ({
             ok: false,
-            message: text,
+            message: text
         }))
     return data
-    
 }
 
-interface Client  {
-    removeIps(options: {ips: string[], zone: string}): Promise<string[]>
-    addIps(options: {count: number, zone: string, zonePassword: string}): Promise<string[]>
-    availableIps(options: {zone: string, zonePassword: string}): Promise<string[]>
-    removeIps(options: {ips: string[], zone: string}): Promise<string[]>
-    refreshIps(options: {ips: string[], zone: string}): Promise<string[]>
+export interface Client {
+    removeIps(options: { ips: string[]; zone: string }): Promise<string[]>
+    addIps(options: {
+        count: number
+        zone: string
+        zonePassword: string
+    }): Promise<string[]>
+    availableIps(options: {
+        zone: string
+        zonePassword: string
+    }): Promise<string[]>
+    removeIps(options: { ips: string[]; zone: string }): Promise<string[]>
+    refreshIps(options: { ips: string[]; zone: string }): Promise<string[]>
 }
 
-
-
-export const createClient = ({
-    email,
-    password,
-    customer,
-}): Client=> {
+export const createClient = ({ email, password, customer }): Client => {
     const defaults = {
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
-        method: 'POST',
+        method: 'POST'
     }
     let client: Client = {} as any
-    client.addIps = async ({count=1, zone, zonePassword}) => { // returns only the added ips
-            if(!zone) {
-                throw new Error('zone is needed')
-            }
-            const previousIps = await client.availableIps({zone, zonePassword})
-            return await fetch('https://luminati.io/api/add_ips', {
-                ...defaults,
-                body: JSON.stringify({
-                    email,
-                    password,
-                    customer,
-                    zone,
-                    count: 1,
-                })
-            }).then(mapErrorToJson)
-            .then(data => {
+    client.addIps = async ({ count = 1, zone, zonePassword }) => {
+        // returns only the added ips
+        if (!zone) {
+            throw new Error('zone is needed')
+        }
+        const previousIps = await client.availableIps({ zone, zonePassword })
+        return await fetch('https://luminati.io/api/add_ips', {
+            ...defaults,
+            body: JSON.stringify({
+                email,
+                password,
+                customer,
+                zone,
+                count: 1
+            })
+        })
+            .then(mapErrorToJson)
+            .then((data) => {
                 if (!data.ok) throw new Error(data.message)
                 return data
             })
-            .then(data => {
+            .then((data) => {
                 const all = data.ips
-                return all.filter(ip => !previousIps.find(x => x==ip))
+                return all.filter((ip) => !previousIps.find((x) => x == ip))
             })
     }
-    client.availableIps = ({zone, zonePassword}) => {
+    client.availableIps = ({ zone, zonePassword }) => {
         // curl "https://luminati.io/api/get_route_ips?" -H "X-Hola-Auth: lum-customer-CUSTOMER-zone-ZONE-key-PASSWORD"
         return fetch('https://luminati.io/api/get_route_ips?expand=1', {
             ...defaults,
@@ -75,8 +73,7 @@ export const createClient = ({
                 'X-Hola-Auth': `lum-customer-${customer}-zone-${zone}-key-${zonePassword}`
             },
             method: 'GET'
-        })
-        .then(async (res) => {
+        }).then(async (res) => {
             const text = await res.text()
             const rx = /(\d+\.\d+\.\d+\.\d+(\n)?)+/
             if (!rx.test(text)) {
@@ -85,36 +82,38 @@ export const createClient = ({
             return text.split('\n')
         })
     }
-    client.removeIps = ({ips, zone}) => { // returns only the removed ips
-            if(!ips) {
-                throw new Error('at least one ip is needed')
-            }
-            if(!zone) {
-                throw new Error('zone is needed')
-            }
-            return fetch('https://luminati.io/api/remove_ips', {
-                ...defaults,
-                body: JSON.stringify({
-                    email,
-                    password,
-                    customer,
-                    zone,
-                    ips,
-                })
-            }).then(mapErrorToJson)
-            .then(data => {
+    client.removeIps = ({ ips, zone }) => {
+        // returns only the removed ips
+        if (!ips) {
+            throw new Error('at least one ip is needed')
+        }
+        if (!zone) {
+            throw new Error('zone is needed')
+        }
+        return fetch('https://luminati.io/api/remove_ips', {
+            ...defaults,
+            body: JSON.stringify({
+                email,
+                password,
+                customer,
+                zone,
+                ips
+            })
+        })
+            .then(mapErrorToJson)
+            .then((data) => {
                 if (!data.ok) throw new Error(data.message)
                 return data
             })
-            .then(data => {
+            .then((data) => {
                 return data.ips
             })
     }
-    client.refreshIps = ({ips, zone}) => {
-        if(!ips) {
+    client.refreshIps = ({ ips, zone }) => {
+        if (!ips) {
             throw new Error('at least one ip is needed')
         }
-        if(!zone) {
+        if (!zone) {
             throw new Error('zone is needed')
         }
         return fetch('https://luminati.io/api/refresh', {
@@ -124,20 +123,18 @@ export const createClient = ({
                 password,
                 customer,
                 zone,
-                ips,
+                ips
             })
-        }).then(mapErrorToJson)
-        .then(data => {
-            if (!data.ok) throw new Error(data.message)
-            return data
         })
-        .then(data => {
-            return data.ips
-        })
+            .then(mapErrorToJson)
+            .then((data) => {
+                if (!data.ok) throw new Error(data.message)
+                return data
+            })
+            .then((data) => {
+                return data.ips
+            })
     }
-    
+
     return client
 }
-
-
-
